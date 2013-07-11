b158090772d597bb31aed0018fb80233e2ce4060
Delyan Raychev <az@delqn.com>
2013-07-11 00:20:26 -0700

    Initial Commit

---
+++ DStar Chat/DStarChatAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  DStarChatAppDelegate.h
+//  DStar Chat
+//
+//  Created by Delyan Raychev on 7/11/13.
+//  Copyright (c) 2013 Delyan Raychev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DStarChatAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ DStar Chat/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DStar Chat
+//
+//  Created by Delyan Raychev on 7/11/13.
+//  Copyright (c) 2013 Delyan Raychev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
